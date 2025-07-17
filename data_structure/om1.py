39c92931ff761349663a3af98c15ee48c57480d6
atharva <atharva.gade24@mmit.edu.in>
2025-07-18 00:05:05 +0530

    om1.py

---
+++ data_structure/om1.py
@@ -0,0 +1,11 @@
+# this code is to demonstrate square calculation
+def sqr():
+    
+    num = int(input(("enter the number:") ))
+    square= num*num
+    print("square of number is",square)
+sqr()    
+
+
+
+
